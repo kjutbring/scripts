5c5f82d67fbd62463314f066cc3a64294cead86f
kjutbring <kittyj@cmail.nu>
2015-05-21 21:27:47 +0200

    set placement for monitor with xrandr

--- scdetect.sh
+++ scdetect.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
 
+# set placement of monitor in relation to laptop.  
+
+while getopts ":l:" opt; do
+    case $opt in
+        l)
+            if [ "$OPTARG" = "right" ]; then
+                if xrandr -q|grep "LVDS1 connected"; then
+                    xrandr --output LVDS1 --auto
+                fi
+                if xrandr -q|grep "VGA1 connected"; then
+                    xrandr --output VGA1 --auto
+                    xrandr --output VGA1 --right-of LVDS1
+                fi
+            fi
+            if [ "$OPTARG" = "left" ]; then
+                if xrandr -q|grep "LVDS1 connected"; then
+                    xrandr --output LVDS1 --auto
+                fi
+                if xrandr -q|grep "VGA1 connected"; then
+                    xrandr --output VGA1 --auto
+                    xrandr --output VGA1 --left-of LVDS1
+                fi
+            fi
+        ;;
+        \?)
+            echo "[-] invalid argument, specify location of additional monitor. l + right/left."
+            exit 1
+        ;;
+        :)
+            if xrandr -q|grep "LVDS1 connected"; then
+                xrandr --output LVDS1 --auto
+            fi
+        ;;
+    esac
+done
 
 
